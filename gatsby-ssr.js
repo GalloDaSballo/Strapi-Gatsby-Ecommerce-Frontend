6546b5ed0e1fdc126b36ad17731abd9c842d9909
Gallo Da Sballo <alessandro@hubmade.com>
2020-05-02 16:25:37 +0200

    Fixed Context Rendering

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,3 +5,11 @@
  */
 
 // You can delete this file if you're not using it
+import React from 'react'
+import CartContextProvider from './src/context/CartContext'
+
+export const wrapRootElement = ({element}) => (
+    <CartContextProvider>
+        {element}
+    </CartContextProvider>
+)
